84da96b25cdd4f18c671d7770e76b362aa266883
Mariia Khan <mkhan@pascal-devbox01.l3s.intra>
2024-06-23 06:01:05 +0200

    new

---
+++ evaluation_script/challenge_1/phase_2.py
@@ -0,0 +1,144 @@
+import random
+import json
+from bleu_cider.pycocoevalcap.eval import eval2
+#import openai
+import re
+'''
+def load_config(file_path):
+    with open(file_path, 'r') as file:
+        return json.load(file)
+
+
+config_file_path = 'config.json'
+
+
+config = load_config(config_file_path)
+
+api_key = config.get('OPENAI_API_KEY')
+if api_key is None:
+    raise ValueError("No OpenAI API key found in config file.")
+'''
+
+
+def load_data(annoation_file):
+    with open(annoation_file, 'r') as f:
+        data = json.load(f)
+
+    return data
+
+
+def build_answer_dict(data):
+    answer_dict = {}
+    for instance in data:
+        name = instance["name"]
+        # Join all attribute values that are not empty into a single string
+        answer = "".join([str(attr["value"]) for attr in instance["attributes"] if attr["value"]])
+        answer_dict[name] = answer
+    return answer_dict
+
+def get_bleu_cider_gpt(test_annotation_file, user_submission_file):
+    gts = load_data(test_annotation_file)
+    res = load_data(user_submission_file)
+
+    gt_answers = build_answer_dict(gts)
+    user_answers = build_answer_dict(res)
+
+    valid_comparisons = []
+
+    for i, (name, gt_answer) in enumerate(gt_answers.items()):
+        user_answer = user_answers.get(name, "")
+        if user_answer == "":
+            print(f"Missing answer for {name}")
+            valid_comparisons.append({"name": name, "gt_answer": gt_answer, "user_answer": ""})
+            continue
+        valid_comparisons.append({"name": name, "gt_answer": gt_answer, "user_answer": user_answer})
+
+    # Prepare data for evaluation
+    gt_eval = {f"{i}": [comp["gt_answer"]] for i, comp in enumerate(valid_comparisons)}
+    user_eval = {f"{i}": [comp["user_answer"]] for i, comp in enumerate(valid_comparisons)}
+    print (type(gt_eval))
+    print ("")
+    print (type(user_eval))
+    result = eval2(gt_eval, user_eval)
+    print ("result", result)
+    # gpt_similarity = get_gpt4_eval(gt_eval, user_eval)
+
+    # return result['bleu'], result['cider'], gpt_similarity
+    return result['bleu'], result['cider']
+
+
+def evaluate_bleu_cider(test_annotation_file, user_submission_file, phase_codename, **kwargs):
+    print("Starting Evaluation.....")
+    """
+    Evaluates the submission for a particular challenge phase and returns score
+    Arguments:
+
+        `test_annotations_file`: Path to test_annotation_file on the server
+        `user_submission_file`: Path to file submitted by the user
+        `phase_codename`: Phase to which submission is made
+
+        `**kwargs`: keyword arguments that contains additional submission
+        metadata that challenge hosts can use to send slack notification.
+        You can access the submission metadata
+        with kwargs['submission_metadata']
+
+        Example: A sample submission metadata can be accessed like this:
+        >>> print(kwargs['submission_metadata'])
+        {
+            'status': u'running',
+            'when_made_public': None,
+            'participant_team': 5,
+            'input_file': 'https://abc.xyz/path/to/submission/file.json',
+            'execution_time': u'123',
+            'publication_url': u'ABC',
+            'challenge_phase': 1,
+            'created_by': u'ABC',
+            'stdout_file': 'https://abc.xyz/path/to/stdout/file.json',
+            'method_name': u'Test',
+            'stderr_file': 'https://abc.xyz/path/to/stderr/file.json',
+            'participant_team_name': u'Test Team',
+            'project_url': u'http://foo.bar',
+            'method_description': u'ABC',
+            'is_public': False,
+            'submission_result_file': 'https://abc.xyz/path/result/file.json',
+            'id': 123,
+            'submitted_at': u'2017-03-20T19:22:03.880652Z'
+        }
+    """
+    output = {}
+    if phase_codename == "dev":
+        print("Evaluating for Dev Phase")
+        bleu, cider = get_bleu_cider_gpt(test_annotation_file, user_submission_file)
+        output["result"] = [
+            {
+                "train_split": {
+                    "BLEU-4": bleu,
+                    "CIDEr": cider,
+                    "Total": (bleu + cider)/2,
+                }
+            }
+        ]
+        # To display the results in the result file
+        output["submission_result"] = output["result"][0]["train_split"]
+        print("Completed evaluation for Dev Phase")
+    elif phase_codename == "cond":
+        print("Evaluating for Test Phase")
+        bleu, cider = get_bleu_cider_gpt(test_annotation_file, user_submission_file)
+        output["result"] = [
+            {
+                "train_split": {
+                    "BLEU-4": bleu,
+                    "CIDEr": cider,
+                    "Total": (bleu + cider)/2,
+                }
+            },
+        ]
+        # To display the results in the result file
+        output["submission_result"] = output["result"][0]
+        print("Completed evaluation for Test Phase")
+    return output
+
+
+if __name__ == '__main__':
+    # evaluate("answer_sheet.json", "test_eval.json", "test")
+    evaluate("../annotations/capt_renamed_test_annotations.json", "test_eval_bleu.json", "cond")
---
+++ evaluation_script/challenge_2/bleu_cider/pycocoevalcap/eval.py
@@ -0,0 +1,47 @@
+from .bleu.bleu import Bleu
+from .cider.cider import Cider
+#from .meteor.meteor import Meteor
+#from .rouge.rouge import Rouge
+#from .spice.spice import Spice
+#from .wmd.wmd import WMD
+import numpy as np
+import json
+
+def eval(gts,res):
+    scorer = Bleu(n=4)
+    s1, _ = scorer.compute_score(gts, res)
+    
+    scorer = Cider()
+    s2, _ = scorer.compute_score(gts, res)
+
+    #scorer = Meteor()
+    #s3, _ = scorer.compute_score(gts, res)
+
+    #scorer = Rouge()
+    #s4, _ = scorer.compute_score(gts, res)
+
+
+    #scorer = Spice()
+    #s5, _ = scorer.compute_score(gts, res)
+    
+
+
+    # scorer = WMD()
+    # s6, _ = scorer.compute_score(gts, res)
+
+    return {'bleu':s1[3],'cider':s2}#,'meteor':s3,'spice':s5 }#,'wmd':s6}
+    #return {'bleu':s1[3], 'cider': int(s2*1000)/0.1, 'meteor': int(s3*1000)/0.3, 'rouge': int(s4*1000)/0.3}#,'cider':s2,'meteor':s3,'rouge':s4}
+
+def get_bleu(gts,res):
+    scorer = Bleu(n=4)
+    s, _ = scorer.compute_score(gts, res)
+    return s[2]
+
+
+
+def get_cider(gts, res):
+    scorer = Cider()
+    s, _ = scorer.compute_score(gts, res)
+    return s
+
+
